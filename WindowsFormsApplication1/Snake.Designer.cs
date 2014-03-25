eb2e742c835280d24713dce7466d095c7f06652d
jeedey93 <justin.do@hotmail.com>
2014-03-25 18:19:56 -0400

    From School Added Stuff

--- WindowsFormsApplication1/Snake.Designer.cs
+++ WindowsFormsApplication1/Snake.Designer.cs
@@ -36,7 +36,6 @@
             this.hrToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -44,12 +43,13 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 67);
+            this.pictureBox1.Location = new System.Drawing.Point(47, 48);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(260, 225);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // menuStrip1
             // 
@@ -93,22 +93,11 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(74, 166);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "RENAUD A CHIER";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Snake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 323);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -135,7 +124,6 @@
         private System.Windows.Forms.ToolStripSeparator hrToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Button button1;
 
     }
 }
--- WindowsFormsApplication1/Snake.cs
+++ WindowsFormsApplication1/Snake.cs
@@ -17,12 +17,15 @@ namespace WindowsFormsApplication1
         public bool gameOver = false;
         List<SnakePart> snake = new List<SnakePart>();
         SnakePart food;
+        int direction = 0;
         Timer gameLoop= new Timer();
         Timer snakeLoop = new Timer();
 
         public Snake()
         {
             InitializeComponent();
+            gameLoop.Tick += new EventHandler(Update);
+            gameLoop.Tick += new EventHandler(UpdateSnake);
             gameLoop.Interval = 1000 / 60;
             snakeLoop.Interval = 1000 / 10;
             gameLoop.Start();
@@ -45,8 +48,13 @@ namespace WindowsFormsApplication1
         }
 
         private void StartGame() 
-        { 
-
+        {
+            gameOver = false;
+            snake.Clear();
+            score = 0;
+            SnakePart head = new SnakePart(10,8);
+            snake.Add(head);
+            GenerateFood();
         }
 
         private void GameOver() 
@@ -54,20 +62,80 @@ namespace WindowsFormsApplication1
 
         }
 
-        private void Update() 
-        { 
-
+        private void Update(object sender, EventArgs e) 
+        {
+            if (gameOver)
+            { }
+            else 
+            {
+                if (Input.Press(Keys.Left))
+                {
+                    if (snake.Count < 2 || snake[0].X == snake[1].X)
+                        direction = 1;
+                }
+                else if (Input.Press(Keys.Right))
+                {
+                    if (snake.Count < 2 || snake[0].X == snake[1].X)
+                        direction = 2;
+                }
+                else if (Input.Press(Keys.Up))
+                {
+                    if (snake.Count < 2 || snake[0].Y == snake[1].Y)
+                        direction = 3;
+                }
+                else if (Input.Press(Keys.Down))
+                {
+                    if (snake.Count < 2 || snake[0].X == snake[1].X)
+                        direction = 0;
+                }
+            }
+                 
         }
 
-        private void Draw(Graphics graphics) 
-        { 
+        private void UpdateSnake(object sender, EventArgs e) 
+        {
+            if (!gameOver)
+            {
 
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void GenerateFood() 
+        {
+            Random random = new Random();
+            food = new SnakePart(random.Next(0, 20), random.Next(0, 15));
+        }
+
+        private void Draw(Graphics canvas) 
+        {
+            Font font = this.Font;
+            if (gameOver) 
+            {
+                SizeF message = canvas.MeasureString("GameOver", font);
+                canvas.DrawString("GameOver", font, Brushes.White, new PointF(160 - message.Width / 2, 120));
+                message = canvas.MeasureString("Final Score" + score.ToString(), font);
+                canvas.DrawString("Final Score " + score.ToString(), font, Brushes.White, new PointF(160 - message.Width / 2, 140));
+                message = canvas.MeasureString("Press Enter to start a new Game", font);
+                canvas.DrawString("Press enter to start a new game", font, Brushes.White, new PointF(160 - message.Width / 2, 160));
+            }
+            else 
+            {
+                canvas.DrawString("Score " + score.ToString(), font, Brushes.Black, new Point(4, 4));
+                for(int i=0; i<snake.Count; i++)
+                {
+                    Color snake_color = i == 0 ? Color.Red : Color.Black;
+                    SnakePart currentpart= snake[i];
+                    canvas.FillRectangle(new SolidBrush(snake_color), new Rectangle(currentpart.X=16, currentpart.Y=16,16,16));
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
-        }
 
+
+
+        }
     }
 }
